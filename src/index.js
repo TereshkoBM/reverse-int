e0a7907695fd0650ca99577aed7fcf865ddb98e0
Boris Tereshko <tbm_mail@tut.by>
2022-01-18 12:35:20 +0300

    fix: update function reverse (n)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-module.exports = function reverse (n) {
-  let result=0;
-  if(n < 0) n = -n;
-  while(n>0){
-  result = (n % 10) + (result * 10);  
-  n= (n - (n % 10)) / 10; 
-  }
-  return result;
-}
+module.exports = function reverse(n) {
+    let result = 0;
+    if (n < 0) n = -n;
+    while (n > 0) {
+        result = (n % 10) + result * 10;
+        n = (n - (n % 10)) / 10;
+    }
+    return result;
+};
